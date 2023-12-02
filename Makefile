aa0182379760275278360aacf6803efc77d6da92
Temi Taylor <rocktalonttaylor@gmail.com>
2023-12-02 16:44:40 -0500

    testing file reading

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 
 .PHONY: all
-all: neuro_cuts
+all: classbench neuro_cuts
 
 neuro_cuts: neuro_cuts_tb.cc neuro_cuts.sv Makefile
 	@echo "SV neuro_cuts.sv"
 	@verilator --cc neuro_cuts.sv --public-flat-rw -Wno-UNOPTFLAT -Wno-UNOPT -Wno-CASEINCOMPLETE -Wno-WIDTH --exe neuro_cuts_tb.cc --build -j --top-module neuro_cuts
 	@mv obj_dir/Vneuro_cuts neuro_cuts
+
+classbench: classbench_parser.cc
+	@echo "classbench_parser"
 
 .PHONY: clean
 clean:
---
+++ classbench_parser.cc
@@ -0,0 +1,43 @@
+#include <time.h>
+#include <memory.h>
+#include <iostream>
+#include <cstdint>
+#include <fstream>
+#include <string>
+
+class ClassBenchLine {
+	public:
+		uint32_t ip_1;
+		uint8_t ip_width_1;
+		uint32_t ip_2;
+		uint8_t ip_width_2;
+
+		uint16_t min_port_1;
+		uint16_t max_port_1;
+		uint16_t min_port_2;
+		uint16_t max_port_2;
+
+		uint8_t first_hex_1;
+		uint8_t second_hex_1;
+		uint16_t first_hex_2;
+		uint16_t second_hex_2;
+
+		ClassBenchLine(std::string line) {
+			printf(line);
+		}
+};
+
+public ClassBenchLine[] parseClassBenchFile(std::string filename) {
+	std::ifstream cbfile (filename);
+	std::string cb_string;
+
+	if (cbfile.isopen()) {
+		cbfile >> cb_string;
+		printf(cb_string);
+	}
+}
+
+int main(int argc, char ** argv)
+{
+    parseClassBenchFile("classbench/acl2_1k");
+}
