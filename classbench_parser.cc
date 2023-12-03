3ffa57d508710db738ab0c2b8397d73c44b71221
Temi Taylor <rocktalonttaylor@gmail.com>
2023-12-03 03:22:36 -0500

    parsing ip

--- classbench_parser.cc
+++ classbench_parser.cc
@@ -1,9 +1,9 @@
-#include <time.h>
-#include <memory.h>
 #include <iostream>
 #include <cstdint>
 #include <fstream>
 #include <string>
+
+using namespace std;
 
 class ClassBenchLine {
 	public:
@@ -22,18 +22,33 @@ class ClassBenchLine {
 		uint16_t first_hex_2;
 		uint16_t second_hex_2;
 
-		ClassBenchLine(std::string line) {
-			std::cout << line;
+		ClassBenchLine(string line) {
+			cout << line << endl;
 		}
 };
 
-ClassBenchLine parseClassBenchFile(std::string filename) {
-	std::ifstream cbfile (filename);
-	std::string cb_string;
+struct ipPair{
+	uint32_t ip;
+	uint8_t ip_width;
+};
+
+struct int8Pair{
+	uint8_t v1;
+	uint8_t v2;
+};
+
+struct int16Pair{
+	uint16_t v1;
+	uint16_t v2;
+};
+
+ClassBenchLine parseClassBenchFile(string filename) {
+	ifstream cbfile (filename);
+	string cb_string;
 
 	if (cbfile.is_open()) {
 		while (cbfile) {
-			std::string ip1, ip2, p1a, p1b, p2a, p2b, hx1, hx2, _; 
+			string ip1, ip2, p1a, p1b, p2a, p2b, hx1, hx2, _; 
 			cbfile >> ip1;
 			cbfile >> ip2;
 			cbfile >> p1a;
@@ -45,11 +60,30 @@ ClassBenchLine parseClassBenchFile(std::string filename) {
 			cbfile >> hx1;
 			cbfile >> hx2;
 			cb_string = ip1 + " " + ip2 + " " + p1a + " " + p1b + " " + p2a + " " + p2b + " " + hx1 + " " + hx2;
+
+			cout << parseIpString(ip1.substr(1, ip1.length())) << endl;
+			cout << parseIpString(ip2) << endl;
+
 			return ClassBenchLine(cb_string);
 		}
 	}
-
 	return ClassBenchLine(cb_string);
+}
+
+ipPair parseIpString(string ipString) {
+	int slash = ip.find("/");
+	string ips = ip.substr(0, slash) + ".";
+	string ipw = ip.substr(slash+1, ip.length());
+
+	uint32_t ip_int = 0;
+	for (int dot = ips.find("."); dot != -1; dot = ips.find(".")) {
+		string ippart = ips.substr(0, dot);
+		ip_int = (ip_int << 8) + stoi(ippart);
+		ips = ips.substr(dot+1, ips.length());
+	}
+
+	ipPair = {ip_int, stoi(ipw)};
+	return ipPair;
 }
 
 int main(int argc, char ** argv)
