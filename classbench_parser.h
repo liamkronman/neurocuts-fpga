cf8596ed8fa2d0af1b265ca34f281f2443a5be9e
Temi Taylor <rocktalonttaylor@gmail.com>
2023-12-05 21:01:32 -0500

    adding Rule class

--- classbench_parser.h
+++ classbench_parser.h
@@ -14,6 +14,21 @@ struct intPair{
 	uint16_t v2;
 };
 
+template<typename T>
+struct valueRange{
+	T start;
+	T end;
+};
+
+struct packet{
+	uint32_t src_ip;
+	uint32_t dst_ip;
+	uint16_t src_port;
+	uint16_t dst_port;
+	uint16_t protocol;
+}
+
+
 class ClassBenchLine {
 	public:
 		uint32_t src_ip;
@@ -29,23 +44,20 @@ class ClassBenchLine {
 		uint16_t protocol;
 		uint16_t protocol_mask;
 
-		ClassBenchLine(ipPair ip1, ipPair ip2, intPair port1, intPair port2, intPair hex1, intPair hex2) {
-			this->ip_1 = ip1.ip;
-			this->ip_width_1 = ip1.ip_width;
-			this->ip_2 = ip2.ip;
-			this->ip_width_2 = ip2.ip_width;
-
-			this->min_port_1 = port1.v1;
-			this->max_port_1 = port1.v2;
-			this->min_port_2 = port2.v1;
-			this->max_port_2 = port2.v2;
-
-			this->first_hex_1 = hex1.v1;
-			this->second_hex_1 = hex1.v2;
-			this->first_hex_2 = hex2.v1;
-			this->second_hex_2 = hex2.v2;
-		}
+		ClassBenchLine(ipPair ip1, ipPair ip2, intPair port1, intPair port2, intPair hex1, intPair hex2);
 };
+
+class Rule {
+	public:
+		valueRange<uint32_t> src_ip;
+		valueRange<uint32_t> dst_ip;
+		valueRange<uint16_t> src_port;
+		valueRange<uint16_t> dst_port;
+		valueRange<uint16_t> protocol;
+
+		Rule(valueRange<uint32_t> src_ip, valueRange<uint32_t> dst_ip, valueRange<uint16_t> src_port, valueRange<uint16_t> dst_port, valueRange<uint16_t> protocol);
+		packet sample();
+}
 
 std::vector<ClassBenchLine> parse_classbench(std::string filename);
 
